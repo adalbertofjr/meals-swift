c7b42608266eb43c8b36ef60d73a2abdd1f04479
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-11-16 21:52:57 -0200

    Initial Commit

---
+++ eggplant-brownie-swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eggplant-brownie-swift3
+//
+//  Created by Adalberto Fernandes Junior on 16/11/16.
+//  Copyright © 2016 Adalberto Fernandes Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
